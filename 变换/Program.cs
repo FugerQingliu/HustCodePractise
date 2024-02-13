1817fa697c5c0ad78b210f4ab7e83021aa19a1ca
FugerQingliu <145846302+FugerQingliu@users.noreply.github.com>
2024-02-13 20:07:08 +0800

    测试负数读取和变换这一道题目的思路
    测试负数读取是因为码力提升计划文档中的样例负数我直接复制过来有问题（我以为是我代码出bug了），结果发现是他的样例有问题。变换这一题还是超时。

--- 变换/Program.cs
+++ 变换/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,55 @@ namespace 变换
 {
     internal class Program
     {
+        struct Transform
+        {
+            public int type;
+            public float value;
+        }
+        struct Node
+        {
+            public int i;
+            public int j;
+            public long x;
+            public long y;
+        }
         static void Main(string[] args)
         {
+            int n = 0, m = 0;
+            string[] input = Console.ReadLine().Split(' ');
+            n = int.Parse(input[0]);
+            m = int.Parse(input[1]);
+            Transform[] transforms = new Transform[n + 1];
+            for(int i = 1; i < n+1;i++)
+            {
+                input = Console.ReadLine().Split(' ');
+                transforms[i].type = int.Parse(input[0]);
+                transforms[i].value = float.Parse(input[1]);
+            }
+            Node[] nodes = new Node[m];
+            for(int i = 0; i< m ;i++)
+            {
+                input = Console.ReadLine().Split(' ');
+                nodes[i].i = int.Parse(input[0]);
+                nodes[i].j = int.Parse(input[1]);
+                nodes[i].x = long.Parse(input[2]);
+                nodes[i].y = long.Parse(input[3]);
+            }
+            for(int i = 0;i<m; i++)
+            {
+                float k = 1f , th = 0;
+                for(int j = nodes[i].i; j <= nodes[i].j; j++)
+                {
+                    switch (transforms[j].type)
+                    {
+                        case 1:
+                            k *= transforms[j].value; break;
+                        case 2:
+                            th += transforms[j].value;break;
+                    }
+                }
+                Console.WriteLine(k*(nodes[i].x * Math.Cos(th) - nodes[i].y * Math.Sin(th)) + " " + k * (nodes[i].x *Math.Sin(th) + nodes[i].y * Math.Cos(th)));
+            }
         }
     }
 }
---
+++ 测试负数读取/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 测试负数读取
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int i = int.Parse(input[0]);
+            int j = int.Parse(input[1]);
+            int n = int.Parse(input[2]);
+            int m = int.Parse(input[3]);
+            Console.WriteLine(i+" " + j+" "+n + " " + m);
+        }
+    }
+}
