712aff273a507555142fed4a408278e399c3bd98
FugerQingliu <145846302+FugerQingliu@users.noreply.github.com>
2024-02-21 23:34:17 +0800

    方程求解
    用了快读，C#可以直接过，有些东西不是很必要，自己想改就改吧。

--- Z-方程求解/Program.cs
+++ Z-方程求解/Program.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.AccessControl;
 
 namespace Z_方程求解
 {
@@ -11,34 +12,90 @@ namespace Z_方程求解
     {
         static void Main(string[] args)
         {
-            int n, q;
+            int n = 0 , q = 0 ;
             string[] input = Console.ReadLine().Split(' ');
             n = int.Parse(input[0]);
             q = int.Parse(input[1]);
-            List<int> answer = new List<int>();
-            for(int i = 0; i < n; i++)
+            HashSet<int> origin = new HashSet<int>();
+            for (int i = 0; i < n; i++)
             {
-                input =Console.ReadLine().Split('x','=');
-                int num = (int.Parse(input[2]) - int.Parse(input[1])) / int.Parse(input[0]);
-                if (!answer.Contains(num))
-                    answer.Add(num);
+                int num1 = 0,num2 = 0,num3 = 0;
+                num1 = QuickRead();
+                num2 = QuickRead();
+                num3 = QuickRead();
+                int temp = (num3 - num2) / num1;
+                if (!origin.Contains(temp))
+                    origin.Add(temp);
             }
-            answer.Sort();
+            int count = origin.Count;
+            int[] ints = origin.ToArray();
+            Array.Sort(ints);
             for(int i = 0; i < q; i++)
             {
-                input = Console.ReadLine().Split(' ');
-                Console.WriteLine(BiggerSearch2(answer, int.Parse(input[1])) - BiggerSearch(answer, int.Parse(input[0])));
+                int cnt = 0;
+                int left = QuickRead();
+                int right = QuickRead();
+                if (right - left < 114514)
+                {
+                    if (count > right - left)
+                    {
+                        for (int j = left; j < right + 1; j++)
+                        {
+                            if (origin.Contains(j))
+                            {
+                                cnt++;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        foreach (int j in origin)
+                        {
+                            if (left <= j && j <= right)
+                            {
+                                cnt++;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    int leftOrder = BiggerOrEqual(ints, left);
+                    int rightOrder = LessOrEqual(ints, right);
+                    if (rightOrder >= leftOrder)
+                        cnt = rightOrder - leftOrder + 1;
+                    else
+                        cnt = 0;
+                }
+                Console.WriteLine(cnt);
             }
         }
-        static int BinarySearch(List<int> array,int value)
+        static int QuickRead()
         {
-            int left = 0 , right = array.Count-1;
-            int middle = (left + right) / 2;
-            while(left < right)
+            int x = 0, f = 1;
+            int ch = Console.Read();
+            while(ch < '0' || ch > '9')
             {
-                if (array[middle] == value)
+                if (ch == '-')
+                    f = -1;
+                ch = Console.Read();
+            }
+            while(ch >= '0' && ch <= '9')
+            {
+                x = x  * 10 + ch - '0';
+                ch = Console.Read();
+            }
+            return x * f;
+        }
+        static int BinarySearch(int[] input, int b)
+        {
+            int left = 0, right = input.Length - 1;
+            int middle = (left + right) / 2;
+            while (left < right)
+            {
+                if (input[middle] == b)
                     return middle;
-                else if (array[middle] < value)
+                else if (input[middle] < b)
                 {
                     left = middle + 1;
                     middle = (left + right) / 2;
@@ -46,50 +103,31 @@ namespace Z_方程求解
                 else
                 {
                     right = middle - 1;
-                    middle = (right + left) / 2;
+                    middle = (left + right) / 2;
                 }
             }
             return middle;
         }
-        static int BiggerSearch(List<int> array,int value)
+        static int BiggerOrEqual(int[] input, int b)
         {
-            int nearAddress = BinarySearch(array, value);
-            if (nearAddress > 0)
-            {
-                while (array[nearAddress] > value)
-                {
-                    nearAddress--;
-                }
-            }
-            if (nearAddress < array.Count - 1 )
-            {
-                while (array[nearAddress] < value)
-                {
-                    nearAddress++;
-                }
-            }
-            return nearAddress;
+            int given = BinarySearch(input, b);
+            while (input[given] >= b && given > 0)
+                given--;
+            if (input[given] >= b)
+                return given;
+            else
+                return given + 1;
         }
-        static int BiggerSearch2(List<int> array, int value)
+        static int LessOrEqual(int[] input, int b)
         {
-            int nearAddress = BinarySearch(array, value);
-            if (nearAddress < array.Count - 1)
-            {
-                while (array[nearAddress] <= value)
-                {
-                    nearAddress++;
-                }
-            }
-            if (nearAddress > 0)
-            {
-                while (array[nearAddress] > value)
-                {
-                    nearAddress--;
-                }
-            }
-            if (array[nearAddress] == value)
-                return nearAddress+1;
-            return nearAddress;
+            int given = BinarySearch(input, b);
+            int len = input.Length - 1;
+            while (input[given] <= b && given < len)
+                given++;
+            if (input[given] <= b)
+                return given;
+            else
+                return given - 1;
         }
     }
 }
